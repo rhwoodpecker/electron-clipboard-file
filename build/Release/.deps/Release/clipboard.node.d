9bc8cede03eb85ceda846b62642078d6780284a5
renhao <rhwoodpecker@163.com>
2023-03-11 12:04:10 +0800

    chore: rm node_modules build

--- build/Release/.deps/Release/clipboard.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/clipboard.node := c++ -bundle -undefined dynamic_lookup -Wl,-no_pie -Wl,-search_paths_first -mmacosx-version-min=10.13 -arch x86_64 -L./Release -stdlib=libc++  -o Release/clipboard.node Release/obj.target/clipboard/clipboard.o -framework CoreFoundation -framework ApplicationServices -framework Carbon
